df6b859d720ee0910f3340918f7379c321c86f19
see2et <seezet1024@gmail.com>
2023-05-16 19:37:25 +0900

    :white_check_mark: Setup Customizing theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import customTheme from "@/theme";
+
+export const theme = extendTheme(customTheme);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
